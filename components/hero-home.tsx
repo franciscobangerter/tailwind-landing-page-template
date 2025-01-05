1d09548fe94fcec5cca0ed921344eb0ef1d6337f
franbangerter <francisco.bangerter@taligent.com.ar>
2025-01-05 02:29:43 -0300

    Update hero-home.tsx

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -70,8 +70,8 @@ export default function HeroHome() {
               data-aos="zoom-y-out"
               data-aos-delay={150}
             >
-              The website builder you're <br className="max-lg:hidden" />
-              looking for
+              Conectamos a las empresas <br className="max-lg:hidden" />
+              con sus próximos clientes
             </h1>
             <div className="mx-auto max-w-3xl">
               <p
